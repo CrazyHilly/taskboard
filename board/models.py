d896ed126156b9a7896db5518beab6713a0beb23
Julia Dudych <juliadudych@gmail.com>
2025-08-15 18:56:44 +0300

    Add Tag and Task models with their fields

--- board/models.py
+++ board/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Tag(models.Model):
+    name = models.CharField(max_length=20, unique=True)
+
+
+class Task(models.Model):
+    name = models.CharField(max_length=60)
+    description = models.TextField(max_length=5000, blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    due_by = models.DateTimeField(blank=True, null=True)
+    completed = models.BooleanField(default=False)
+    completion_time = models.DateTimeField(blank=True, null=True)
+    in_progress = models.BooleanField(default=False)
+    tag = models.ManyToManyField(Tag, related_name="tasks")
