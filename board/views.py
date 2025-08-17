5cf1588e22d2ef40ba93c501897eeef6f9a21769
Julia Dudych <juliadudych@gmail.com>
2025-08-17 17:55:11 +0300

    Add basic views

--- board/views.py
+++ board/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views import generic
 
-# Create your views here.
+from board.forms import TaskCreateForm
+from board.models import Task, Tag
+
+
+class TaskListView(generic.ListView):
+    model = Task
+
+
+class TaskDetailView(generic.DetailView):
+    model = Task
+
+
+class TaskCreateView(generic.CreateView):
+    model = Task
+    form_class = TaskCreateForm
+    success_url = reverse_lazy("board:task-list")
+
+
+class TagListView(generic.ListView):
+    model = Tag
