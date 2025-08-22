b50ce7d8cf31d51e699dce3cce05d885272c124d
Julia Dudych <juliadudych@gmail.com>
2025-08-22 19:46:56 +0300

    Add crispy forms to settings

--- taskboard/settings.py
+++ taskboard/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "crispy_forms",
+    "crispy_bootstrap4",
     "board",
 ]
 
@@ -67,6 +69,8 @@ TEMPLATES = [
         },
     },
 ]
+
+CRISPY_TEMPLATE_PACK = "bootstrap4"
 
 WSGI_APPLICATION = "taskboard.wsgi.application"
 
